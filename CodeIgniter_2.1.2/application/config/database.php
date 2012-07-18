38c2a4a77e943e572da95aea4007a25424b0443f
DanielF <1704249@student.swin.edu.au>
2012-07-18 13:19:43 +1000

    database connection
    I've updated my config and database files to suit my local environment,
    I'm not sure how we are going to handle this. I can't find an easy way
    to ignore these files. Has anyone else looked at this?
    
    Also, I have added some entries to the .gitignore file that are specific
    to codeigniter. I've just cut and past from a post I found. I haven't
    looked at the particulars though.

--- CodeIgniter_2.1.2/application/config/database.php
+++ CodeIgniter_2.1.2/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'cmlac';
+$db['default']['password'] = 'australia2012';
+$db['default']['database'] = 'cmlac';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
