026423c2bdde0bb71cc7621b25fae4cee58467ea
DanielF <1704249@student.swin.edu.au>
2012-09-16 17:07:38 +1000

    Added autoload helpers and libraries

--- CodeIgniter_2.1.2/application/config/autoload.php
+++ CodeIgniter_2.1.2/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database', 'session', 'email', 'validation');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'html');
+$autoload['helper'] = array('url', 'html', 'form', 'text', 'date', 'security');
 
 
 /*
