7c4ef95d210c80f1adc1c3a21951edfefbc29ba2
DanielF <1704249@student.swin.edu.au>
2012-09-16 12:13:20 +1000

    update

--- CodeIgniter_2.1.2/application/controllers/membersHome.php
+++ CodeIgniter_2.1.2/application/controllers/membersHome.php
@@ -7,6 +7,16 @@
 		}
 		public function home()
 		{
+			
+			$this->load->view("members_header_view");
+			$this->load->view("members_nav_view");
+			$this->load->view("members_content_view");
+			$this->load->view("members_footer_view");
+		}
+		public function news()
+		{
+			$this->load->model("members_model");
+			$this->members_model->getNews();
 			$this->load->view("members_header_view");
 			$this->load->view("members_nav_view");
 			$this->load->view("members_content_view");
--- CodeIgniter_2.1.2/application/models/members_model.php
+++ CodeIgniter_2.1.2/application/models/members_model.php
@@ -2,7 +2,8 @@
 	class Members_model extends CI_Model{
 		function getNews()
 			{
-				$query = $this->db->get("club_news",);
+				$this->db->select_max('news_date_created'); //gets the latest news
+				$query = $this->db->get("club_news",5); //gets the 5 most recent
 				return $query->result();
 			}
 	
